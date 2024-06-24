3b0bc7cfac3db50d331d1db84c33010996ea2dbc
_Kerman <kermanx@qq.com>
2024-06-24 16:18:46 +0800

    fix: `getExampleFile`

--- extension/getExampleFile.ts
+++ extension/getExampleFile.ts
@@ -2,10 +2,19 @@ import { basename } from 'node:path'
 import { RelativePattern, Uri, workspace } from 'vscode'
 
 export async function getExampleFile(grammarUri: Uri): Promise<Uri | undefined> {
-  const maybeExamples = await workspace.findFiles(new RelativePattern(
-    Uri.joinPath(grammarUri, '..'),
-    basename(grammarUri.fsPath).replace(/(\.tmLanguage)?\.json$/i, '.example.*'),
-  ))
-
-  return maybeExamples[0]
+  const grammarDirectory = Uri.joinPath(grammarUri, '..')
+  const grammarBasename = basename(grammarUri.fsPath)
+  const exmapleFilenames = [
+    grammarBasename.replace(/(\.tmLanguage)?\.json$/i, '.example'),
+    grammarBasename.replace(/(\.tmLanguage)?\.json$/i, '.example.*'),
+    grammarBasename.replace(/(\.tmLanguage)?\.json$/i, '.*'),
+  ]
+  for (const filename of exmapleFilenames) {
+    const exampleUri = (await workspace.findFiles(
+      new RelativePattern(grammarDirectory, filename),
+      new RelativePattern(grammarDirectory, grammarBasename),
+    ))[0]
+    if (exampleUri)
+      return exampleUri
+  }
 }
